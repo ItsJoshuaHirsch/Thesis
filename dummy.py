2b76670f6c317b576b6cf06ab3820172facc3675
Joshua Hirsch <40173207+ItsJoshuaHirsch@users.noreply.github.com>
2019-01-04 09:51:57 +0100

    Improvements, new Trends, save directly as json
    nibp, hasReanimation, has Shocks

--- dummy.py
+++ dummy.py
@@ -6,6 +6,7 @@ import pprint
 import numpy as np
 import matplotlib.pyplot as plt
 import names
+import os
 
 
 from datetime import datetime, timedelta, timezone
@@ -18,16 +19,28 @@ def main():
     missions = []
     text = ""
     f = open("missions" + str(number_Missions//1000) + "k.txt", "w")
+    # f = open('missions' + str(number_Missions//1000) + 'k.json', 'w')
     for i in range(number_Missions):
+        # print(toJson(randomMission()) + ",")
         # text += toJson(randomMission()) + "\n"
+        # CONTINUE, als JSON speichern, dauert länger, vielleicht aber besser? ohne str() liefs
+        # json.dump(randomMission(), f)
         f.write(toJson(randomMission()) + "\n")
         # missions.append(randomMission())
+    f.close()
+    thisFile = "missions" + str(number_Missions//1000) + "k.txt"
+    base = os.path.splitext(thisFile)[0]
+    os.rename(thisFile, base + ".json")
+    # json.dump(missions, f)
+    # pprint.pprint(missions)
+    # print(missions)
 
 
 def randomMission():
     uuid = randomUUID()
     devicetype = getDeviceType()
     gender = getSex()
+    # firstname, lastname = str(name).split()
     date = datetimeToUTC(randomDate()) * 1000
     mission = {
         "missionUuid": uuid,
@@ -37,8 +50,16 @@ def randomMission():
             "DEVICE_TYPE": devicetype,
             "PATIENT_SEX": gender.upper(),
             "PATIENT_FIRST_NAME": names.get_first_name(gender=gender) if gender != "unknown" else names.get_first_name(),
-            # "PATIENT_SURNAME": names.get_last_name(),
+            # "PATIENT_FIRST_NAME": firstname,
+            # "PATIENT_SURNAME": lastname,
             "PATIENT_AGE": getAge(),
+            "TRENDS_HR_MEAN": float(random.randrange(35, 130)),
+            "TRENDS_PI_MEAN": float(random.randrange(0, 20)),
+            "TRENDS_SPCO_MEAN": float(random.randrange(0, 20)),
+            "TRENDS_SPHB_MEAN": float(random.randrange(3, 16)),
+            "TRENDS_SPMET_MEAN": float(random.randrange(0, 8)),
+            "TRENDS_SPO_2_MEAN": float(random.randrange(80, 100)),
+            "NIBP_COUNT": random.randrange(0, 12),
             "DEVICE_VERSION": getVersion(devicetype),
             "MISSION_START_DATE": {
                 "ts_utc": date,
@@ -46,6 +67,9 @@ def randomMission():
                 "ts_local": date,
                 "daytime": 49081000
             },
+            "CO_2_TREND_AVAILABLE": bool(random.getrandbits(1)) if devicetype != "CORPULS_CPR" else "",
+            "SHOCK_AVAILABLE": bool(random.getrandbits(1)) if devicetype != "CORPULS_CPR" else "",
+            "CPR_FEEDBACK_AVAILABLE": bool(random.getrandbits(1)) if devicetype != "CORPULS_CPR" else "",
 
         }
     }
@@ -102,6 +126,13 @@ def randomAgeSet():
     return ageList
 
 
+""" def nameSet(gender):
+    nameSet = []
+    for i in range(number_Missions+1)
+        names.append(names.get_full_name(
+            gender=gender) if gender != "unknown" else names.get_full_name()),
+
+names = nameSet() """
 ages = randomAgeSet()
 
 
